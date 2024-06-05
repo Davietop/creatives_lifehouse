4624b6074f4df68a5e6d894f4018f813e3bbd198
davietop <oyatoyedavid@gmail.com>
2024-06-05 20:32:56 +0100

    testing

--- components/blogPosts.jsx
+++ components/blogPosts.jsx
@@ -122,7 +122,7 @@ export default function BlogPosts() {
                 router.push(`/post/${clicked.dataset.tab}`);
                 setTimeout(() => {
                   location.reload();
-                }, 300);
+                }, 400);
 
                 if (!clicked) return;
               }}
